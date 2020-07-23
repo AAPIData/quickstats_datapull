2d53216da3291ed886222019d5df911a1cc3e571
SunnyYShao <yshao006@ucr.edu>
2020-07-23 16:35:19 -0700

    update cvap script

--- code/cvap_data_pull.R
+++ code/cvap_data_pull.R
@@ -7,9 +7,35 @@
 library(tidyverse)
 library(stringr)
 
-data <- read_csv("cvap_raw/Nation.csv")
+data_us <- read_csv("cvap_raw/Nation.csv") %>% mutate(geography = "national")
+data_st <- read_csv("cvap_raw/State.csv") %>% mutate(geography = "state")
+data_ct <- read_csv("cvap_raw/County.csv") %>% mutate(geography = "county")
+data_cd <- read_csv("cvap_raw/CD.csv") %>% mutate(geography = "district")
 
-data_clean <- function(){
-  final_dta <- data %>%
-    filter(lnnumber %in% c("4", "6"))
-}
+data <- rbind(data_us, data_st, data_ct, data_cd)
+
+
+final_dta <- data %>%
+  filter(lnnumber %in% c("4", "6")) %>% 
+  rename(NAME = geoname) %>% 
+  mutate(group = case_when(
+    lntitle == "Asian Alone" ~"Asian American Alone",
+    lntitle == "Native Hawaiian or Other Pacific Islander Alone" ~"NHPI Alone")) %>% 
+  mutate(estimate = tot_est,
+         pct = case_when(
+           tot_est > 0 ~round(cvap_est/tot_est, digits = 3),
+           TRUE ~NA_real_)) %>% 
+  mutate(reliable = case_when(
+    cvap_moe <= 0.5 * cvap_est ~"YES",
+    TRUE ~"NO")) %>% 
+  mutate(estimate_reliable = case_when(
+    reliable == "YES" ~estimate,
+    TRUE ~NA_real_),
+         pct_reliable = case_when(
+    reliable == "YES" ~pct,
+    TRUE ~NA_real_)) %>% 
+  mutate(topic = "cvap",
+         label = "CVAP") %>% 
+select(NAME, topic, group, geography, label, estimate, pct, reliable, estimate_reliable, pct_reliable)
+
+write_csv(final_dta, "acs_database/cvap_dta.csv", na = "")
